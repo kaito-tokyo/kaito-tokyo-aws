3a5d7c567109995935d5d2fcffbd83eab298015c
Kaito Udagawa <umireon@gmail.com>
2024-11-10 06:18:15 +0900

    Update create.sh

--- GitHubActionsSelfHostedAssumeRoles/create.sh
+++ GitHubActionsSelfHostedAssumeRoles/create.sh
@@ -9,8 +9,8 @@ aws cloudformation create-stack-set \
   --permission-model=SERVICE_MANAGED \
   --call-as=DELEGATED_ADMIN
 
-aws cloudformation create-stack-instances \
-  --stack-set-name=GitHubActionsSelfHostedObsChatTalkerAssumeRolesDev001 \
-  --deployment-targets=Accounts=586794439382,OrganizationalUnitIds=ou-03d8-tru29wjm,AccountFilterType=INTERSECTION \
-  --call-as=DELEGATED_ADMIN \
-  --regions=us-east-1
+# aws cloudformation create-stack-instances \
+#   --stack-set-name=GitHubActionsSelfHostedObsChatTalkerAssumeRolesDev001 \
+#   --deployment-targets=Accounts=586794439382,OrganizationalUnitIds=ou-03d8-tru29wjm,AccountFilterType=INTERSECTION \
+#   --call-as=DELEGATED_ADMIN \
+#   --regions=us-east-1
