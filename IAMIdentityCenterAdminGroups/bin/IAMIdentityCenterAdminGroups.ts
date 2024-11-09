9ec0d36b55127f4e14a9f583a5026e9159b4c9f6
Kaito Udagawa <umireon@gmail.com>
2024-11-09 23:18:02 +0900

    Fix

--- IAMIdentityCenterAdminGroups/bin/IAMIdentityCenterAdminGroups.ts
+++ IAMIdentityCenterAdminGroups/bin/IAMIdentityCenterAdminGroups.ts
@@ -1,28 +1,37 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { ImportedControlTowerGroupsStack } from "../lib/ImportedControlTowerGroupsStack";
-import { MainStack } from "../lib/MainStack";
 
-const app = new cdk.App();
+import { ImportedControlTowerGroupsStack } from "../lib/ImportedControlTowerGroupsStack.js";
+import { MainStack } from "../lib/MainStack.js";
+
+import { IdentitystoreRepository } from "../lib/IdentitystoreRepository.js";
 
 const env = {
 	account: "784179762749",
 	region: "us-east-1"
 };
 
+const identityStoreId = "d-90674974be";
+
+const identitystoreRepository = new IdentitystoreRepository(env.region, identityStoreId);
+const users = await identitystoreRepository.listUsers();
+
+const app = new cdk.App();
+
 const importedControlTowerGroups = new ImportedControlTowerGroupsStack(
 	app,
 	"IAMIdentityCenterAdminGroupsImportedControlTowerGroupsStack",
 	{
 		env,
-		identityStoreId: "d-90674974be"
+		identityStoreId
 	}
 );
 
 new MainStack(app, "IAMIdentityCenterAdminGroupsMainStack", {
 	env,
 	importedControlTowerGroups,
+	users
 });
 
 app.synth();
--- IAMIdentityCenterAdminGroups/lib/MainStack.ts
+++ IAMIdentityCenterAdminGroups/lib/MainStack.ts
@@ -3,23 +3,39 @@ import { Construct } from "constructs";
 
 import { aws_identitystore as identitystore } from "aws-cdk-lib";
 
-import { ImportedControlTowerGroupsStack } from "./ImportedControlTowerGroupsStack";
+import { ImportedControlTowerGroupsStack } from "./ImportedControlTowerGroupsStack.js";
+
+import { type User } from "@aws-sdk/client-identitystore";
 
 interface MainStackProps extends cdk.StackProps {
 	readonly importedControlTowerGroups: ImportedControlTowerGroupsStack;
+	readonly users: User[];
 }
 
 export class MainStack extends cdk.Stack {
+	findUserIdByEmail(users: User[], email: string): string {
+		const user = users.find((user) => {
+			const PrimaryEmail = user.Emails?.find((Email) => Email.Primary);
+			return PrimaryEmail?.Value === email;
+		});
+		if (!user?.UserId) {
+			throw new Error(`User not found by email: ${email}`);
+		}
+		return user.UserId;
+	}
+
 	constructor(scope: Construct, id: string, props: MainStackProps) {
 		super(scope, id, props);
 
-		const { importedControlTowerGroups } = props;
+		const { importedControlTowerGroups, users } = props;
+
+		const umireonUserId = this.findUserIdByEmail(users, "umireon@kaito.tokyo");
 
 		new identitystore.CfnGroupMembership(this, "UmireonControlTowerAdmins", {
 			groupId: importedControlTowerGroups.controlTowerAdmins.attrGroupId,
 			identityStoreId: importedControlTowerGroups.identityStoreId,
 			memberId: {
-				userId: "748804e8-70e1-708a-56f1-820f4ba90171"
+				userId: umireonUserId
 			}
 		});
 	}
