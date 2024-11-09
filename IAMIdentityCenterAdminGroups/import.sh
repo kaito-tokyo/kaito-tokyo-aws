0a69d550c24c6abd3c19ca883b6d64c4441478d7
Kaito Udagawa <umireon@gmail.com>
2024-11-09 21:27:26 +0900

    Fix

--- IAMIdentityCenterAdminGroups/import.sh
+++ IAMIdentityCenterAdminGroups/import.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npx cdk import -m resource-mapping-ImportedControlTowerGroupsStack.json ImportedControlTowerGroupsStack
+npx cdk import -m resource-mapping-ImportedControlTowerGroupsStack.json IAMIdentityCenterAdminGroupsImportedControlTowerGroupsStack
