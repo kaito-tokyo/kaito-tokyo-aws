9cfe6f1c1abcdd31809170580e9ce694416c834d
Kaito Udagawa <umireon@gmail.com>
2024-11-09 21:45:22 +0900

    Update IAMIdentityCenterAdminGroups.ts

--- IAMIdentityCenterAdminGroups/bin/IAMIdentityCenterAdminGroups.ts
+++ IAMIdentityCenterAdminGroups/bin/IAMIdentityCenterAdminGroups.ts
@@ -2,19 +2,27 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { ImportedControlTowerGroupsStack } from "../lib/ImportedControlTowerGroupsStack";
+import { MainStack } from "../lib/MainStack";
 
 const app = new cdk.App();
 
-new ImportedControlTowerGroupsStack(
+const env = {
+	account: "784179762749",
+	region: "us-east-1"
+};
+
+const importedControlTowerGroups = new ImportedControlTowerGroupsStack(
 	app,
 	"IAMIdentityCenterAdminGroupsImportedControlTowerGroupsStack",
 	{
-		env: {
-			account: "784179762749",
-			region: "us-east-1"
-		},
+		env,
 		identityStoreId: "d-90674974be"
 	}
 );
+
+new MainStack(app, "IAMIdentityCenterAdminGroupsMainStack", {
+	env,
+	importedControlTowerGroups,
+});
 
 app.synth();
