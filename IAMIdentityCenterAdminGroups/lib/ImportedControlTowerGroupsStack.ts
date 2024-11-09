c3c47be7b33ce5cff55a66034507c4f416dd40c5
Kaito Udagawa <umireon@gmail.com>
2024-11-09 20:56:44 +0900

    Fix

--- IAMIdentityCenterAdminGroups/lib/ImportedControlTowerGroupsStack.ts
+++ IAMIdentityCenterAdminGroups/lib/ImportedControlTowerGroupsStack.ts
@@ -22,37 +22,37 @@ export class ImportedControlTowerGroupsStack extends cdk.Stack {
 
 		const { identityStoreId } = props;
 
-		this.accountFactory = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.accountFactory = new identitystore.CfnGroup(this, "AccountFactory", {
 			displayName: "AWSAccountFactory",
 			identityStoreId
 		});
 
-		this.logArchiveViewers = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.logArchiveViewers = new identitystore.CfnGroup(this, "LogArchiveViewers", {
 			displayName: "AWSLogArchiveViewers",
 			identityStoreId
 		});
 
-		this.serviceCatalogAdmins = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.serviceCatalogAdmins = new identitystore.CfnGroup(this, "ServiceCatalogAdmins", {
 			displayName: "AWSServiceCatalogAdmins",
 			identityStoreId
 		});
 
-		this.securityAuditors = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.securityAuditors = new identitystore.CfnGroup(this, "SecurityAuditors", {
 			displayName: "AWSSecurityAuditors",
 			identityStoreId
 		});
 
-		this.logArchiveAdmins = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.logArchiveAdmins = new identitystore.CfnGroup(this, "LogArchiveAdmins", {
 			displayName: "AWSLogArchiveAdmins",
 			identityStoreId
 		});
 
-		this.auditAccountAdmins = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.auditAccountAdmins = new identitystore.CfnGroup(this, "AuditAccountAdmins", {
 			displayName: "AWSAuditAccountAdmins",
 			identityStoreId
 		});
 
-		this.securityAuditPowerUsers = new identitystore.CfnGroup(this, "ControlTowerAdmins", {
+		this.securityAuditPowerUsers = new identitystore.CfnGroup(this, "SecurityAuditPowerUsers", {
 			displayName: "AWSSecurityAuditPowerUsers",
 			identityStoreId
 		});
