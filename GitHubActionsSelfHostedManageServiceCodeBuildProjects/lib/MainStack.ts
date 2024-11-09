f8ab10437487a556cbd705061eb1e38531cc7253
Kaito Udagawa <umireon@gmail.com>
2024-11-10 01:41:47 +0900

    Fix

--- GitHubActionsSelfHostedManageServiceCodeBuildProjects/lib/MainStack.ts
+++ GitHubActionsSelfHostedManageServiceCodeBuildProjects/lib/MainStack.ts
@@ -1,7 +1,10 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
+import { aws_codebuild as codebuild, aws_iam as iam } from "aws-cdk-lib";
+
 import { ImportedCodeConnectionStack } from "./ImportedCodeConnectionStack.js";
+import { KaitoTokyoCodeBuildProject } from "./KaitoTokyoCodeBuildProject.js";
 
 interface MainStackProps extends cdk.StackProps {
 	readonly importedCodeConnection: ImportedCodeConnectionStack;
@@ -10,9 +13,59 @@ interface MainStackProps extends cdk.StackProps {
 export class MainStack extends cdk.Stack {
 	readonly importedCodeConnection: ImportedCodeConnectionStack;
 
+	createCodeConnectionManagedPolicy(
+		importedCodeConnection: ImportedCodeConnectionStack
+	): Iam.ManagedPolicy {
+		return new iam.ManagedPolicy(this, "CodeConnectionManagedPolicy", {
+			statements: [
+				new iam.PolicyStatement({
+					effect: iam.Effect.ALLOW,
+					resources: [
+						importedCodeConnection.githubCodeConnection.attrConnectionArn,
+						importedCodeConnection.githubCodeStarConnection.attrConnectionArn
+					],
+					actions: [
+						"codeconnections:GetConnection",
+						"codeconnections:GetConnectionToken",
+						"codeconnections:UseConnection",
+						"codestar-connections:GetConnection",
+						"codestar-connections:GetConnectionToken"
+					]
+				})
+			]
+		});
+	}
+
+	createSource(repo: string): codebuild.Source {
+		return codebuild.Source.gitHub({
+			owner: "kaito-tokyo",
+			webhook: true,
+			webhookFilters: [
+				codebuild.FilterGroup.inEventOf(
+					codebuild.EventAction.WORKFLOW_JOB_QUEUED
+				).andRepositoryNameIs(repo)
+			]
+		});
+	}
+
 	constructor(scope: Construct, id: string, props: MainStackProps) {
 		super(scope, id, props);
 
 		this.importedCodeConnection = props.importedCodeConnection;
+
+		const codeConnectionManagedPolicy = this.createCodeConnectionManagedPolicy(
+			this.importedCodeConnection
+		);
+		const source = this.createSource("kaito-tokyo-aws");
+
+		new KaitoTokyoCodeBuildProject(
+			this,
+			"GitHubActionsSelfHostedManageServiceCodeBuildProjectsProject",
+			{
+				projectName: "ManageServiceCodeBuildProjects",
+				source,
+				codeConnectionManagedPolicy
+			}
+		);
 	}
 }
