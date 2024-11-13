e353dd593da831bbc3ea372f81e4575847712f1f
Kaito Udagawa <umireon@gmail.com>
2024-11-14 08:05:07 +0900

    Update create-Route53.sh

--- Infrastructure/create-Route53.sh
+++ Infrastructure/create-Route53.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")" || exit 1
 
 npx aws-cdk bootstrap \
-  --trust arn:aws:iam::147997151289:role/Route53ProdRole \
-  --cloudformation-execution-policies arn:aws:iam::147997151289:role/InfrastructureRoute53ProdRole
+  --trust arn:aws:iam::147997151289:role/InfrastructureRoute53ProdRole \
+  --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
