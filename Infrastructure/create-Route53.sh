51f78d2c7ed48ca825a73658ba42007859c89896
Kaito Udagawa <umireon@gmail.com>
2024-11-14 09:37:18 +0900

    Fix

--- Infrastructure/create-Route53.sh
+++ Infrastructure/create-Route53.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npx aws-cdk bootstrap \
+npm run cdk -- bootstrap \
   --trust arn:aws:iam::147997151289:role/KaitoTokyoAwsMainRole \
   --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess
--- Infrastructure/update.sh
+++ Infrastructure/update.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")" || exit 1
 
-npx aws-cdk deploy --require-approval never Route53Stack
+npm run cdk -- deploy --require-approval never Route53Stack
