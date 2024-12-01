769e40e2fb11220d6d7d0fdd21f6896baccccce5
Kaito Udagawa <umireon@gmail.com>
2024-12-01 22:48:33 +0900

    Update cdk.ts

--- CICD/lib/cdk.ts
+++ CICD/lib/cdk.ts
@@ -14,7 +14,6 @@ export function createPolicyStatementForCDKDeploy(
 	return new iam.PolicyStatement({
 		effect: iam.Effect.ALLOW,
 		resources: [
-			`arn:aws:iam::${accountId}:role/cdk-${qualifier}-cfn-exec-role-${accountId}-${region}`,
 			`arn:aws:iam::${accountId}:role/cdk-${qualifier}-deploy-role-${accountId}-${region}`,
 			`arn:aws:iam::${accountId}:role/cdk-${qualifier}-file-publishing-role-${accountId}-${region}`,
 			`arn:aws:iam::${accountId}:role/cdk-${qualifier}-image-publishing-role-${accountId}-${region}`,
