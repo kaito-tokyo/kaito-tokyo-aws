71ecb034bcb60322ed8e152c669e416c19ad824e
Kaito Udagawa <umireon@gmail.com>
2024-12-31 07:23:39 +0900

    Update Route53Stack.ts

--- Infrastructure/lib/Route53Stack.ts
+++ Infrastructure/lib/Route53Stack.ts
@@ -47,9 +47,13 @@ export class Route53Stack extends cdk.Stack {
 		);
 		obsChatTalkerZone.grantDelegation(apidevObsChatTalkerZoneDelegationRole);
 
-		const kaitoTokyoClickZone = route53.HostedZone.fromHostedZoneAttributes(this, "KaitoTokyoClickZone", {
-			zoneName: "click.kaito.tokyo",
-			hostedZoneId: "Z02227982TKVA5OZ2METT"
-		})
+		const kaitoTokyoClickZone = route53.HostedZone.fromHostedZoneAttributes(
+			this,
+			"KaitoTokyoClickZone",
+			{
+				zoneName: "click.kaito.tokyo",
+				hostedZoneId: "Z02227982TKVA5OZ2METT"
+			}
+		);
 	}
 }
