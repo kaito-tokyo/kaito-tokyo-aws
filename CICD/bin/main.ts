7e97d58aad2e2023a4104fe57a132ec3fd095c19
Kaito Udagawa <umireon@gmail.com>
2024-12-31 10:26:58 +0900

    Fix

--- CICD/bin/main.ts
+++ CICD/bin/main.ts
@@ -6,6 +6,7 @@ import { ImportedCodeConnectionStack } from "kaito-tokyo-aws-commonstacks";
 import { ObsChatTalkerBuilderStack } from "../lib/ObsChatTalkerBuilderStack.js";
 
 import { infrastructureAccountIds, workloadsAccountIds } from "kaito-tokyo-aws-commonparameters";
+import { GamingVPNBuilderStack } from "../lib/GamingVPNBuilderStack.js";
 import { InfrastructureBuilderStack } from "../lib/InfrastructureBuilderStack.js";
 
 const app = new cdk.App();
@@ -48,6 +49,20 @@ new ObsChatTalkerBuilderStack(app, "ObsChatTalkerBuilderStack", {
 	cdkDeployTargetEnrionments: [
 		{
 			account: workloadsAccountIds.obsChatTalkerDev001,
+			region: "us-east-1"
+		}
+	]
+});
+
+new GamingVPNBuilderStack(app, "GamingVPNBuilderStack", {
+	env: {
+		account: infrastructureAccountIds.cicdProd001,
+		region: "us-east-1"
+	},
+	importedCodeConnection,
+	cdkDeployTargetEnrionments: [
+		{
+			account: workloadsAccountIds.gamingVPNProd001,
 			region: "us-east-1"
 		}
 	]
